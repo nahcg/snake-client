9437160fff0c062a1fb1d21dd73a3996aa4725bb
nahcg <glor.chan@mail.utoronto.ca>
2023-05-19 14:35:17 -0400

    add connect function

---
+++ client.js
@@ -0,0 +1,18 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost", // IP address here
+    port: 50541, // PORT number here
+    on: (data) => {
+      console.log(data);
+    },
+  });
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+
+    return conn;
+};
+
+module.exports = connect;
