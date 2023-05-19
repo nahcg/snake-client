a35de3b834a9f90a2054652b5eedca22f7b62aeb
nahcg <glor.chan@mail.utoronto.ca>
2023-05-19 14:35:30 -0400

    export connect function

--- play.js
+++ play.js
@@ -1,20 +1,5 @@
-const net = require("net");
+const connect = require("./client.js");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost", // IP address here
-    port: 50541, // PORT number here
-    on: (data) => {
-      console.log(data);
-    },
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
