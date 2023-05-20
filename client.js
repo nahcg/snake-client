2325469023ebd42256ac2d9e06dd2716d6181c77
nahcg <glor.chan@mail.utoronto.ca>
2023-05-19 22:10:41 -0400

    testing move up command

--- client.js
+++ client.js
@@ -9,9 +9,11 @@ const connect = function () {
     conn.on("data", (data) => {
       console.log(data);
     }),
+    // connection event
     conn.on("connect", () => {
       console.log("Successfully connected to game server")
-      conn.write("Name: SNK")
+      conn.write("Name: SNK") // name command
+      // conn.write("Move: up") // move up command
     }),
 
     // interpret incoming data as text
