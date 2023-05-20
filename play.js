604c7e8f60e8c386118a273fe5db3ae4f11ba45e
nahcg <glor.chan@mail.utoronto.ca>
2023-05-20 13:29:51 -0400

    require setupInput function

--- play.js
+++ play.js
@@ -1,22 +1,7 @@
 const connect = require("./client.js");
+const setupInput = require("./input.js");
 
 console.log("Connecting ...");
 connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput); // event listener for stdin
-  // function runs when it receives input from keyboard
-  const handleUserInput = function (data) {
-    if (data === "ctrl+c") {
-      console.log("Client ended game: ", data)
-      process.exit()
-    }
-  };
-  return stdin;
-};
+setupInput();
