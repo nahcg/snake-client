ad6f0c5372370fea99b133524ae3403d0fecd137
nahcg <glor.chan@mail.utoronto.ca>
2023-05-20 11:28:19 -0400

    add function to receive input from keyboard

--- play.js
+++ play.js
@@ -1,6 +1,22 @@
 const connect = require("./client.js");
 
-
 console.log("Connecting ...");
 connect();
 
+// setup interface to handle user input from stdin
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput); // event listener for stdin
+  // function runs when it receives input from keyboard
+  const handleUserInput = function (data) {
+    if (data === "ctrl+c") {
+      console.log("Client ended game: ", data)
+      process.exit()
+    }
+  };
+  return stdin;
+};
